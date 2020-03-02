72acb5c696cb1c00e38b5e8252aef5f5a26864e2
istiqomahnf <istiqomahnurfatayati@gmail.com>
2020-03-02 09:12:12 +0700

    yuhu

--- app/Grade.php
+++ app/Grade.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Grade extends Model
 {
     protected $table = 'grade';
-
+  
     protected $fillable = ['grade','updated_at', 'created_at'];
 
     public function student(){
